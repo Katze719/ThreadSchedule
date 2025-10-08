770936f182ce11aa20b56e7b148bc6eea84b4037
Katze719 <pauldorn1234@gmail.com>
2025-10-08 23:09:50 +0200

    Refactor ThreadWrapperReg, JThreadWrapperReg, and PThreadWrapperReg constructors to use decltype for parameter forwarding and remove generic constructors to prevent misuse. This enhances type safety and clarity in thread registration.

--- include/threadschedule/registered_threads.hpp
+++ include/threadschedule/registered_threads.hpp
@@ -3,10 +3,13 @@
 #include "pthread_wrapper.hpp"
 #include "thread_registry.hpp"
 #include "thread_wrapper.hpp"
+#include <string>
 #include <utility>
 
 namespace threadschedule
 {
+
+//
 
 // Registered std::thread wrapper (opt-in)
 class ThreadWrapperReg : public ThreadWrapper
@@ -17,24 +20,15 @@ class ThreadWrapperReg : public ThreadWrapper
     template <typename F, typename... Args>
     explicit ThreadWrapperReg(std::string name, std::string componentTag, F &&f, Args &&...args)
         : ThreadWrapper(
-              [n = std::move(name), c = std::move(componentTag), func = std::forward<F>(f)](Args... inner) {
+              [n = std::move(name), c = std::move(componentTag), func = std::forward<F>(f)](auto &&...inner) {
                   AutoRegisterCurrentThread guard(n, c);
-                  func(std::forward<Args>(inner)...);
+                  func(std::forward<decltype(inner)>(inner)...);
               },
               std::forward<Args>(args)...)
     {
     }
 
-    template <typename F, typename... Args>
-    explicit ThreadWrapperReg(F &&f, Args &&...args)
-        : ThreadWrapper(
-              [func = std::forward<F>(f)](Args... inner) {
-                  AutoRegisterCurrentThread guard;
-                  func(std::forward<Args>(inner)...);
-              },
-              std::forward<Args>(args)...)
-    {
-    }
+    // No generic constructor without name/tag to avoid accidental misuse
 };
 
 #if __cplusplus >= 202002L
@@ -46,24 +40,15 @@ class JThreadWrapperReg : public JThreadWrapper
     template <typename F, typename... Args>
     explicit JThreadWrapperReg(std::string name, std::string componentTag, F &&f, Args &&...args)
         : JThreadWrapper(
-              [n = std::move(name), c = std::move(componentTag), func = std::forward<F>(f)](Args... inner) {
+              [n = std::move(name), c = std::move(componentTag), func = std::forward<F>(f)](auto &&...inner) {
                   AutoRegisterCurrentThread guard(n, c);
-                  func(std::forward<Args>(inner)...);
+                  func(std::forward<decltype(inner)>(inner)...);
               },
               std::forward<Args>(args)...)
     {
     }
 
-    template <typename F, typename... Args>
-    explicit JThreadWrapperReg(F &&f, Args &&...args)
-        : JThreadWrapper(
-              [func = std::forward<F>(f)](Args... inner) {
-                  AutoRegisterCurrentThread guard;
-                  func(std::forward<Args>(inner)...);
-              },
-              std::forward<Args>(args)...)
-    {
-    }
+    // No generic constructor without name/tag to avoid accidental misuse
 };
 #endif
 
@@ -76,24 +61,15 @@ class PThreadWrapperReg : public PThreadWrapper
     template <typename F, typename... Args>
     explicit PThreadWrapperReg(std::string name, std::string componentTag, F &&f, Args &&...args)
         : PThreadWrapper(
-              [n = std::move(name), c = std::move(componentTag), func = std::forward<F>(f)](Args... inner) {
+              [n = std::move(name), c = std::move(componentTag), func = std::forward<F>(f)](auto &&...inner) {
                   AutoRegisterCurrentThread guard(n, c);
-                  func(std::forward<Args>(inner)...);
+                  func(std::forward<decltype(inner)>(inner)...);
               },
               std::forward<Args>(args)...)
     {
     }
 
-    template <typename F, typename... Args>
-    explicit PThreadWrapperReg(F &&f, Args &&...args)
-        : PThreadWrapper(
-              [func = std::forward<F>(f)](Args... inner) {
-                  AutoRegisterCurrentThread guard;
-                  func(std::forward<Args>(inner)...);
-              },
-              std::forward<Args>(args)...)
-    {
-    }
+    // No generic constructor without name/tag to avoid accidental misuse
 };
 #endif
 
